8e252ed30e74e94679c4c5fb0d8120ba6a54bb83
Erick Geraldeli <erickvitoriogeraldeli@gmail.com>
2023-08-06 16:00:09 -0300

    endpoint /pedidos /{id} disponibilizados

--- src/main/java/com/example/demo/domain/ItemPedido.java
+++ src/main/java/com/example/demo/domain/ItemPedido.java
@@ -3,6 +3,8 @@ package com.example.demo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 
@@ -10,9 +12,9 @@ import jakarta.persistence.Entity;
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
 	
 	
 	private Double desconto;
@@ -31,6 +33,7 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
