6981a2f9fd17420df8b5466bd99f839cd0e276fd
Erick Geraldeli <erickvitoriogeraldeli@gmail.com>
2023-08-06 19:31:23 -0300

    Correção nas exceçoes nos endpoints

--- src/main/java/com/example/demo/services/ClienteService.java
+++ src/main/java/com/example/demo/services/ClienteService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.Categoria;
 import com.example.demo.domain.Cliente;
 import com.example.demo.repositories.ClienteRepository;
 import com.example.demo.services.exceptions.ObjectNotFoundException;
